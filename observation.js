f4bffc1f254445a3199ee0aed7c81d4eecdb50db
张尧 <yao.zhang@datayes.com>
2019-07-22 18:46:46 +0800

    add observation.js

---
+++ observation.js
@@ -0,0 +1,41 @@
+// 观察者模式
+// 要素：观察者，被观察者列表，通知，被观察者响应
+
+class Observation {
+  observerList = [];
+
+  observe(id, react) {
+    if (this.observerList.some(({ id }) => id === id)) return;
+    if (react instanceof Function) {
+      this.observerList.push({ [id]: react });
+    }
+  }
+
+  unObserve(unobserveId) {
+    this.observerList = this.observerList.filter(
+      ({ id }) => id !== unobserveId
+    );
+  }
+
+  notice() {
+    if (this.observerList.toString() !== "[]") {
+      this.observerList.forEach(react => {
+        react();
+      });
+    }
+  }
+}
+
+const ob = new Observation();
+
+ob.observe("ein", () => {
+  console.log("it's ein");
+});
+
+ob.observe("zwei", () => {
+  console.log("i'm watching u");
+});
+
+ob.notice();
+
+ob.unObserve("ein");
